5a37b44369a1c2d9e265a3003ec8d83b6090d7f4
Rune M. Andersen <rune.andersen@gmail.com>
2014-03-19 11:13:57 +0100

    Add cause to exception.

--- src/no/wtw/android/restserviceutils/RestServiceException.java
+++ src/no/wtw/android/restserviceutils/RestServiceException.java
@@ -41,13 +41,13 @@ public class RestServiceException extends IOException {
             return (RestServiceException) e;
 
         if (e instanceof HttpMessageConversionException)
-            return new RestServiceException(HttpStatus.NO_CONTENT, "Could not parse content");
+            return new RestServiceException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not parse content", e);
 
         if (e instanceof ResourceAccessException)
-            return new RestServiceException(HttpStatus.SERVICE_UNAVAILABLE, "Could not access remote server");
+            return new RestServiceException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not access remote server", e);
 
         if (e instanceof RestClientException)
-            return new RestServiceException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown client error");
+            return new RestServiceException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown client error", e);
 
         return new RestServiceException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
     }
