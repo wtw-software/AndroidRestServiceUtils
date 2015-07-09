e160cb4f7b9b561177884c74cd12a65de114d646
Rune M. Andersen <rune.andersen@wtw.no>
2015-07-09 09:08:54 +0200

    Make default request interceptor do logging.

--- src/no/wtw/android/restserviceutils/RestServiceRequestInterceptor.java
+++ src/no/wtw/android/restserviceutils/RestServiceRequestInterceptor.java
@@ -1,14 +1,17 @@
 package no.wtw.android.restserviceutils;
 
+import android.text.TextUtils;
 import android.util.Log;
+import com.google.gson.*;
+import com.google.gson.stream.JsonWriter;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpAuthentication;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.*;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.Locale;
 
@@ -20,12 +23,14 @@ public class RestServiceRequestInterceptor implements ClientHttpRequestIntercept
     public RestServiceRequestInterceptor(RestServiceAPI api) {
         super();
         this.api = api;
+        this.api.getRestTemplate().setRequestFactory(
+                new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory())
+        );
     }
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] data, ClientHttpRequestExecution execution) throws IOException {
         Log.d(TAG, request.getMethod().name() + ": " + request.getURI().toString());
-
         HttpHeaders headers = request.getHeaders();
         headers.setUserAgent(UserAgentFormatter.getUserAgent(api.getContext()));
         headers.setDate(new Date().getTime());
@@ -33,7 +38,37 @@ public class RestServiceRequestInterceptor implements ClientHttpRequestIntercept
         HttpAuthentication authentication = api.getAuthentication();
         if (authentication != null)
             headers.setAuthorization(authentication);
-        return execution.execute(request, data);
+
+        ClientHttpResponse response = execution.execute(request, data);
+        logRequest(request, data, response);
+        return response;
+    }
+
+    private void logRequest(HttpRequest request, byte[] data, ClientHttpResponse response) throws IOException {
+        Log.d(TAG, "Data: " + new String(data));
+        for (Object key : request.getHeaders().keySet())
+            Log.d(TAG, "<" + key + ">: " + request.getHeaders().get(key));
+        if (response != null) {
+            Log.d(TAG, "Status: " + response.getStatusCode() + " " + response.getStatusText());
+            if (response.getBody() != null)
+                Log.d(TAG, "Response: \n" + prettyJson(IOUtils.toString(response.getBody())) + "\n");
+        }
+    }
+
+    public static String prettyJson(String body) {
+        if (TextUtils.isEmpty(body))
+            return body;
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            StringWriter stringWriter = new StringWriter();
+            JsonWriter jsonWriter = new JsonWriter(stringWriter);
+            jsonWriter.setIndent("\u00A0\u00A0");
+            JsonElement jsonElement = new JsonParser().parse(body);
+            gson.toJson(jsonElement, jsonWriter);
+            return stringWriter.toString();
+        } catch (JsonParseException e) {
+            return body;
+        }
     }
 
 }
