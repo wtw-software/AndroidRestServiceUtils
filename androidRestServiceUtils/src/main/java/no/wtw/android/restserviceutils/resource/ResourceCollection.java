538255bae6cf929a2b1fc6331d689b671f108f28
Rune M. Andersen <rune.andersen@wtw.no>
2017-04-28 14:37:51 +0200

    Require resource collections to be sortable.

--- androidRestServiceUtils/src/main/java/no/wtw/android/restserviceutils/resource/ResourceCollection.java
+++ androidRestServiceUtils/src/main/java/no/wtw/android/restserviceutils/resource/ResourceCollection.java
@@ -2,14 +2,16 @@ package no.wtw.android.restserviceutils.resource;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Collections;
 import java.util.List;
 
-public class ResourceCollection<D> extends Resource {
+public class ResourceCollection<D extends Comparable<D>> extends Resource {
 
     @SerializedName("resource")
     private List<D> resources;
 
     public List<D> getResources() {
+        Collections.sort(resources);
         return resources;
     }
 
