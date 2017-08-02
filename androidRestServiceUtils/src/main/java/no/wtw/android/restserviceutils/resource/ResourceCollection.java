fed23b2068d5589004b984d9d25af0929f6e476d
Rune M. Andersen <rune.andersen@wtw.no>
2017-08-02 12:47:44 +0200

    Better implementation of sorting.

--- androidRestServiceUtils/src/main/java/no/wtw/android/restserviceutils/resource/ResourceCollection.java
+++ androidRestServiceUtils/src/main/java/no/wtw/android/restserviceutils/resource/ResourceCollection.java
@@ -2,7 +2,6 @@ package no.wtw.android.restserviceutils.resource;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,11 +10,14 @@ public class ResourceCollection<D extends Comparable<? super D>> extends Resourc
     @SerializedName("resource")
     private List<D> resources;
 
+    private boolean isSorted;
+
     public List<D> getResources() {
-        // Prevent java.util.ConcurrentModificationException by creating a new temp list for each time.
-        List<D> newList = new ArrayList<>(resources);
-        Collections.sort(newList);
-        return newList;
+        if (!isSorted) {
+            Collections.sort(resources);
+            isSorted = true;
+        }
+        return resources;
     }
 
 }
