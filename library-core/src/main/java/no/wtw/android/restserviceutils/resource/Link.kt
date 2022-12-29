0fe6c651a9b22edca029ad7ae4c842ae11dc8ebf
Rune M. Andersen <rune.andersen@gmail.com>
2022-12-29 12:15:25 +0100

    Refactored RSA.

--- library-core/src/main/java/no/wtw/android/restserviceutils/resource/Link.kt
+++ library-core/src/main/java/no/wtw/android/restserviceutils/resource/Link.kt
@@ -1,8 +1,13 @@
 package no.wtw.android.restserviceutils.resource
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import no.wtw.android.restserviceutils.exceptions.LinkNotResolvedException
 import no.wtw.android.restserviceutils.exceptions.RestServiceException
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.springframework.http.HttpStatus
 import java.io.Serializable
 
 class Link<T> : Serializable {
@@ -42,6 +47,125 @@ class Link<T> : Serializable {
     fun setClass(clazz: Class<T>?) {
         this.clazz = clazz
     }
+
+    fun httpGet(client: OkHttpClient, gson: Gson): T {
+        val qp = queryParams
+        if (qp != null)
+            return httpGet(client, gson, qp)
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            val request = Request.Builder().url(url!!).get().build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                _resource = gson.fromJson(body, clazz)
+                _resource ?: throw RestServiceException("Body is null")
+            }
+        }
+    }
+
+    fun httpGet(client: OkHttpClient, gson: Gson, queryParams: Map<String, String>): T {
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            var queryString = ""
+            for (key in queryParams.keys)
+                queryString += key + "=" + queryParams[key] + "&"
+            val request = Request.Builder().url(url!! + "?" + queryString.substring(0, queryString.length - 1)).get().build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                _resource = gson.fromJson(body, clazz)
+                _resource ?: throw RestServiceException("Body is null")
+            }
+        }
+    }
+
+    fun httpGet(client: OkHttpClient, gson: Gson, query: JsonEncodedQuery): T {
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            val url = url!!.replace("?data=Base64", "") // TODO: remove this hack
+            val request = Request.Builder().url(url + "?data=" + query.encode(true)).get().build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                _resource = gson.fromJson(body, clazz)
+                _resource ?: throw RestServiceException("Body is null")
+            }
+        }
+    }
+
+    fun httpGet(client: OkHttpClient, gson: Gson, urlAppendix: String): T {
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            val request = Request.Builder().url(url!! + urlAppendix).get().build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                _resource = gson.fromJson(body, clazz)
+                _resource ?: throw RestServiceException("Body is null")
+            }
+        }
+    }
+
+    fun httpPut(client: OkHttpClient, gson: Gson, data: T): T {
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            val jsonData = data?.let { gson.toJson(it) } ?: ""
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+            val requestBody: RequestBody = jsonData.toRequestBody(mediaType)
+            val request = Request.Builder().url(url!!).put(requestBody).build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                _resource = gson.fromJson(body, clazz)
+                _resource ?: throw RestServiceException("Body is null")
+            }
+        }
+    }
+
+    fun httpPost(client: OkHttpClient, gson: Gson, data: Any?): T {
+        return executeHttpCall {
+            if (clazz == null)
+                throw RuntimeException("Class of return object must be set")
+            val jsonData = data?.let { gson.toJson(it) } ?: ""
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+            val requestBody: RequestBody = jsonData.toRequestBody(mediaType)
+            val request = Request.Builder().url(url!!).post(requestBody).build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+                gson.fromJson(body, clazz)
+            }
+        }
+    }
+
+    fun httpDelete(client: OkHttpClient, gson: Gson) =
+        executeHttpCall {
+            val request = Request.Builder().url(url!!).delete().build()
+            client.newCall(request).execute().use { response ->
+                val body = response.body?.string() ?: ""
+                response.body?.close()
+                if (!response.isSuccessful)
+                    throw RestServiceException(HttpStatus.valueOf(response.code), body)
+            }
+        }
 
     private fun <RT> executeHttpCall(block: () -> RT): RT {
         return try {
--- library/src/main/java/no/wtw/android/restserviceutils/RestServiceAPI.kt
+++ library/src/main/java/no/wtw/android/restserviceutils/RestServiceAPI.kt
@@ -3,10 +3,12 @@ package no.wtw.android.restserviceutils
 import android.content.Context
 import android.net.ConnectivityManager
 import android.util.Log
+import com.google.gson.Gson
 import no.wtw.android.restserviceutils.RestServiceAPI
 import no.wtw.android.restserviceutils.exceptions.RestServiceException
 import no.wtw.android.restserviceutils.resource.JsonEncodedQuery
 import no.wtw.android.restserviceutils.resource.Link
+import okhttp3.OkHttpClient
 import org.springframework.http.*
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
@@ -91,19 +93,14 @@ abstract class RestServiceAPI<S> {
         private val TAG = RestServiceAPI::class.java.simpleName
     }
 
-    abstract fun <T> httpGet(link: Link<T>): T
-    abstract fun <T> httpGet(link: Link<T>, query: JsonEncodedQuery): T
-    abstract fun <T> httpGet(link: Link<T>, queryParams: Map<String, String>): T
-    abstract fun <T> httpGet(link: Link<T>, urlAppendix: String): T
-    abstract fun <T> httpPut(link: Link<T>, body: T): T
-    abstract fun <T> httpPost(link: Link<T>, body: Any?): T
-    abstract fun <T> httpDelete(link: Link<T>)
+    abstract fun getClient(): OkHttpClient
+    abstract fun getGson(): Gson
 
 }
 
-fun <T> Link<T>.httpGet(api: RestServiceAPI<*>): T = api.httpGet(this)
-fun <T> Link<T>.httpGet(api: RestServiceAPI<*>, queryParams: Map<String, String>): T = api.httpGet(this, queryParams)
-fun <T> Link<T>.httpGet(api: RestServiceAPI<*>, query: JsonEncodedQuery): T = api.httpGet(this, query)
-fun <T> Link<T>.httpDelete(api: RestServiceAPI<*>): Unit = api.httpDelete(this)
-fun <T> Link<T>.httpPut(api: RestServiceAPI<*>, body: T): T = api.httpPut(this, body)
-fun <T> Link<T>.httpPost(api: RestServiceAPI<*>, body: Any?): T = api.httpPost(this, body)
+fun <T> Link<T>.httpGet(api: RestServiceAPI<*>): T = this.httpGet(api.getClient(), api.getGson())
+fun <T> Link<T>.httpGet(api: RestServiceAPI<*>, queryParams: Map<String, String>): T = this.httpGet(api.getClient(), api.getGson(), queryParams)
+fun <T> Link<T>.httpGet(api: RestServiceAPI<*>, query: JsonEncodedQuery): T = this.httpGet(api.getClient(), api.getGson(), query)
+fun <T> Link<T>.httpDelete(api: RestServiceAPI<*>): Unit = this.httpDelete(api.getClient(), api.getGson())
+fun <T> Link<T>.httpPut(api: RestServiceAPI<*>, body: T): T = this.httpPut(api.getClient(), api.getGson(), body)
+fun <T> Link<T>.httpPost(api: RestServiceAPI<*>, body: Any?): T = this.httpPost(api.getClient(), api.getGson(), body)
